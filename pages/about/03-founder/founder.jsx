adff536cbe7fe922519d9c59a94e8780fcf8d1b3
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-06-15 17:06:58 +0700

    Add content to founder section. 1 Misc:   + Footer gets increased margin top.   + Include h5 in global reset.   + Cloudinary baseUrl default quality set to 'eco'. 2 Founder section:   + Fully build out component.   + Create content data inside about-data.

--- pages/about/03-founder/founder.jsx
+++ pages/about/03-founder/founder.jsx
@@ -1,11 +1,66 @@
 // External lib
 import React from 'react'
 
+// Internal lib
+import { founderText } from '@/data/about-data'
+import { baseUrl } from '@/utils/cloudinary'
+
 // Styling
-import { founder } from './founder.module.scss'
+import {
+  founder,
+  sectionHero,
+  sectionTitle,
+  founderImg,
+  quote as quoteStyle,
+  founderTextContent,
+  qualifications,
+  title,
+  list,
+  item,
+  blurb as blurbStyle
+} from './founder.module.scss'
+import { QuoteMark } from '@/elements/svg/quote-mark'
 
 export const Founder = () => {
+  const { quote, qual, blurb } = founderText
+
   return (
-    <section className={founder}>Founder</section>
+    <section className={founder}>
+      <div className={sectionHero}>
+        <span className={founderImg}>
+          <img
+            src={baseUrl('founder-ph_tujxvc', 'good')}
+            alt='founder of ELI, Alison Brzenchek'
+          />
+        </span>
+        <div className={quoteStyle}>
+          <span><QuoteMark /></span>
+          <p>{quote.text}</p>
+          <em>{quote.attr}</em>
+        </div>
+        <div className={sectionTitle}>
+          <h2>About the Founder</h2>
+          <h4>Alison Brzenchek</h4>
+        </div>
+      </div>
+      <div className={founderTextContent}>
+        <div className={qualifications}>
+          <h5 className={title}>{qual.title}</h5>
+          <ul className={list}>
+            {qual.list.map((qlf, idx) => {
+              return (
+                <li key={idx} className={item}>
+                  {qlf}
+                </li>
+              )
+            })}
+          </ul>
+        </div>
+        <p className={blurbStyle}>
+          <b>{blurb.emph}{' '}</b>
+          {blurb.text}
+        </p>
+      </div>
+    </section>
   )
 }
--- utils/cloudinary.js
+++ utils/cloudinary.js
@@ -1,4 +1,4 @@
-export const baseUrl = (slug, quality='good', fileType='png') => {
+export const baseUrl = (slug, quality='eco', fileType='png') => {
   
   return `https://res.cloudinary.com/jameswalker-work/image/upload/f_auto,q_auto:${quality}/v1654597814/ELI/${slug}.${fileType}`
 }
