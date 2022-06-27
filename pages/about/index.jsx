64d030bbaf0238257eb64213a0d835c44810c239
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2022-06-27 18:56:51 +0700

    Relocate page sections; about page styling refactor.

--- pages/about/index.jsx
+++ pages/about/index.jsx
@@ -2,9 +2,9 @@
 import React from 'react'
 
 // Components
-import { Hero } from '../../components/page-sections/about/01-hero/hero'
-import { Intro } from './02-intro/intro'
-import { Founder } from './03-founder/founder'
+import { Hero } from '@/page-sections/about/01-hero/hero'
+import { Intro } from '@/page-sections/about/02-intro/intro'
+import { Founder } from '@/page-sections/about/03-founder/founder'
 
 const About = () => {
   return (
--- pages/index.jsx
+++ pages/index.jsx
@@ -2,13 +2,13 @@
 import React from 'react'
 
 // Components
-import { Hero } from '../components/page-sections/home/01-hero/hero.jsx'
-import { Intro } from '../components/page-sections/home/02-intro/intro.jsx'
-import { Offerings } from '../components/page-sections/home/03-offerings/offerings'
-import { Pillars } from '../components/page-sections/home/04-pillars/pillars'
-import { Testimonial } from '../components/page-sections/home/05-testimonial/testimonial'
-import { Contact } from '../components/page-sections/home/07-contact/contact'
-import { Scheduling } from '../components/page-sections/home/06-scheduling/scheduling'
+import { Hero } from '@/page-sections/home/01-hero/hero.jsx'
+import { Intro } from '@/page-sections/home/02-intro/intro.jsx'
+import { Offerings } from '@/page-sections/home/03-offerings/offerings'
+import { Pillars } from '@/page-sections/home/04-pillars/pillars'
+import { Testimonial } from '@/page-sections/home/05-testimonial/testimonial'
+import { Contact } from '@/page-sections/home/07-contact/contact'
+import { Scheduling } from '@/page-sections/home/06-scheduling/scheduling'
 
 
 
