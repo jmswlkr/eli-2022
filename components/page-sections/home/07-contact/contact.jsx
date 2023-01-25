aaa96d12ea3db49e482e42091a1fdee7755a6369
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-01-25 23:33:08 +0700

    Update contact section. + Remove map. + Add Make an appointment section.

--- components/page-sections/home/07-contact/contact.jsx
+++ components/page-sections/home/07-contact/contact.jsx
@@ -4,25 +4,26 @@ import { send } from '@emailjs/browser'
 
 // Components
 import { ArrowBtn } from 'components/elements/arrow-btn/arrow-btn'
+import { SectionHeader } from '@/elements/section-header'
 
 // Styling
 import {
   contact,
   formSection,
   blurb,
-  sectionAccent,
-  title,
-  text,
   contactForm,
+  contactInfo,
   name,
   email,
   submitBtn,
   message,
-  mapSection,
-  mapShade,
+  appointment,
+  image,
+  cta,
+  heading,
+  text,
+  btn,
 } from './contact.module.scss'
-import { WaveCircles } from '@/elements/svg/wave-circles'
-import { SectionHeader } from '@/elements/section-header'
 
 export const Contact = () => {
   const [isMobile, setIsMobile] = useState(false)
@@ -67,50 +68,57 @@ export const Contact = () => {
   }
 
   return (
-    <section className={contact} id='contact' >
+    <section className={contact} id='contact'>
       <div className={formSection}>
         <div className={blurb}>
-          <SectionHeader title='Contact us.' withLabel={false}/>
-          {/* <div className={sectionAccent}>
-            <WaveCircles />
-          </div>
-          <h2 className={title}>Contact us.</h2> */}
-          <p className={text}>
-            If you have additional questions use the form below to contact us
-            directly.{' '}
-          </p>
+          <SectionHeader title='Get in touch.' withLabel={false} />
         </div>
         <form className={contactForm}>
-          <input
-            className={name}
-            type='text'
-            name='from_name'
-            placeholder='Name'
-            // value={toSend.from_name}
-            onChange={handleChange}
-          />
-          <input
-            className={email}
-            type='text'
-            name='reply_to'
-            placeholder='Email'
-            // value={toSend.reply_to}
-            onChange={handleChange}
-          />
+          <div className={contactInfo}>
+            <input
+              className={name}
+              type='text'
+              name='from_name'
+              placeholder='Name'
+              // value={toSend.from_name}
+              onChange={handleChange}
+            />
+            <input
+              className={email}
+              type='text'
+              name='reply_to'
+              placeholder='Email'
+              // value={toSend.reply_to}
+              onChange={handleChange}
+            />
+          </div>
           <textarea
             className={message}
             // value={toSend.mess}
             onChange={handleChange}
             placeholder='Enter your message...'
           />
-          <span className={submitBtn}>
-            <ArrowBtn text='Sumbit' btnAction={handleSubmit} />
-          </span>
         </form>
+        <span className={submitBtn}>
+          <ArrowBtn text='Sumbit' btnAction={handleSubmit} />
+        </span>
       </div>
-      <div className={mapSection}>
-        <div className={mapShade} />
-        <img src={isMobile ? '/map.png' : '/map.png'} alt='map of location' />
+      <div className={appointment}>
+        <div className={image}>
+          <img
+            src='https://res.cloudinary.com/jameswalker-work/image/upload/v1666932113/ELI/talk_tx1so3.png'
+            alt='therapist and patient discussing in exposed brick office.'
+          />
+        </div>
+        <div className={cta}>
+          <h4 className={heading}>Ready for more?</h4>
+          <div className={text}>
+            Get to know us with a free initial consult online.
+          </div>
+          <div className={btn}>
+            <ArrowBtn text='Book now' btnAction={() => {}} />
+          </div>
+        </div>
       </div>
     </section>
   )
