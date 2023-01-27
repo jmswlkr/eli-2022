5a2275d1c587e9be61178c05c572014964d8caf7
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-01-27 19:14:44 +0700

    Reorder testimonials; update text.

--- ancillary/small-data.js
+++ ancillary/small-data.js
@@ -86,56 +86,27 @@ export const contactInfo = [
 export const testimonialData = [
   {
     path: '/',
-    text: <b>Compassionate.</b>,
+    text: <b>Compassionate</b>,
     videoThumb:
       'https://res.cloudinary.com/jameswalker-work/image/upload/v1674480597/ELI/alyona_a9urqh.png',
     videoUrl:
       'https://res.cloudinary.com/jameswalker-work/video/upload/v1674480601/ELI/Alyona__compressed_mj9qve.mp4',
-    name: 'Alyona',
+    name: 'Alyona K.',
   },
   {
     path: '/',
-    text: (
-      <>
-        <b>Insightful.</b>
-        <span>
-          Alison is excellent at facilitating insightful group discussion. She
-          brought a deep sense of acceptance and compassion that allowed group
-          participants to explore challenging topics in a safe and supportive
-          context.
-        </span>
-      </>
-    ),
-    name: 'Gary M.',
-  },
-  {
-    path: '/',
-    text: (
-      <>
-        <b>Life Changing.</b>{' '}
-        <span>
-          Alison’s workshop helped me to spend less time in my head and more
-          time in my body. It was a life-changing experience that has enabled me
-          to be a more present, authentic, and grounded leaders.
-        </span>
-      </>
-    ),
-    name: 'Diane W.',
-  },
-  {
-    path: '/',
-    text: <b>Attentive.</b>,
+    text: <b>Attentive</b>,
     videoThumb:
       'https://res.cloudinary.com/jameswalker-work/image/upload/v1674479639/ELI/maria_lvifjh.png',
     videoUrl:
       'https://res.cloudinary.com/jameswalker-work/video/upload/v1674479393/ELI/Maria__compressed_zwtmci.mp4',
-    name: 'Maria',
+    name: 'Maria A.',
   },
   {
     path: '/',
     text: (
       <>
-        <b>Transformational.</b>{' '}
+        <b>Transformational</b>{' '}
         <span>
           Working with Alison was transformational. She's incredibly
           knowledgeable, open, and compassionate. Her deep listening and
@@ -150,14 +121,29 @@ export const testimonialData = [
     path: '/',
     text: (
       <>
-        <b>Brilliant.</b>{' '}
+        <b>Insightful</b>
         <span>
-          Pretium diam ut lacinia interdum. Praesent tincidunt iaculis nisl at
-          dapibus. Etiam vehicula pretium mi ut rhoncus. Mauris faucibus eros
-          nec tristique volutpat.
+          Alison is excellent at facilitating insightful group discussion. She
+          brought a deep sense of acceptance and compassion that allowed group
+          participants to explore challenging topics in a safe and supportive
+          context.
         </span>
       </>
     ),
-    name: 'Andrew W.',
+    name: 'Gary M.',
+  },
+  {
+    path: '/',
+    text: (
+      <>
+        <b>Life Changing</b>{' '}
+        <span>
+          Alison’s workshop helped me to spend less time in my head and more
+          time in my body. It was a life-changing experience that has enabled me
+          to be a more present, authentic, and grounded leader.
+        </span>
+      </>
+    ),
+    name: 'Diane W.',
   },
 ]
