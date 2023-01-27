04e12f1b017054e7d95ee849eed399ea75b17ad3
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-01-27 22:26:56 +0700

    Offerings section elements renamed to "intro".

---
+++ components/page-sections/home/02-intro/intro-data.js
@@ -0,0 +1,26 @@
+export const cards = [
+  {
+    // imgSrc: 'founder-ph_tujxvc',
+    imgSrc: 'path-girl_wsbslm',
+    title: 'About ELI',
+    text: 'Learn more about ELI’s founder, mission, vision, and values.',
+    btnText: 'Learn more',
+    path: '/about',
+  },
+  {
+    // imgSrc: 'consulting_b8zqih',
+    imgSrc: 'stone-stack_ztdlzn',
+    title: 'ELI Offerings',
+    text: 'Explore our coaching, consulting, and collaborative learning services.',
+    btnText: 'Learn More',
+    path: '/offerings',
+  },
+  {
+    // imgSrc: 'talk-lg_v21sfo',
+    imgSrc: 'forest-stairs_wsa4ut',
+    title: 'Free Initial Consult',
+    text: ' Book a free 30 minute consultation to learn more.',
+    btnText: 'Schedule now',
+    path: '/calendar',
+  },
+]
--- pages/index.jsx
+++ pages/index.jsx
@@ -6,14 +6,14 @@ import { Hero } from '@/page-sections/home/01-hero/hero.jsx'
 import { Pillars } from '@/page-sections/home/04-pillars/pillars'
 import { Testimonial } from '@/page-sections/home/05-testimonial/testimonial'
 import { Contact } from '@/page-sections/home/07-contact/contact'
-import { OfferingsSection } from '@/page-sections/offerings/index';
+import { IntroSection } from '@/page-sections/home/02-intro/index';
 
 export default function Home() {
 
   return (
     <>
       <Hero />
-      <OfferingsSection />
+      <IntroSection />
       <Pillars />
       <Testimonial />
       <Contact />
