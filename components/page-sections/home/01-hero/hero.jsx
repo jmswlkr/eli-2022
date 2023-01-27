4c1dde44016d7dc19cd92badc4dc16b5a9712324
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-01-27 22:39:47 +0700

    Update hero video; add new button design.

--- components/page-sections/home/01-hero/hero.jsx
+++ components/page-sections/home/01-hero/hero.jsx
@@ -25,6 +25,7 @@ import {
   btn,
   fadeTransitionBar,
 } from './hero.module.scss'
+import { Button } from '@/elements/general-btn/general-btn'
 
 export const Hero = () => {
   const [fontsLoaded, setFontsLoaded] = useState(false)
@@ -59,7 +60,8 @@ export const Hero = () => {
           <video
             key='img1'
             className={bg}
-            src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1663676344/ELI/beach_tlfnut.mp4`}
+            // src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1663676344/ELI/beach_tlfnut.mp4`}
+            src={`https://res.cloudinary.com/jameswalker-work/video/upload/v1674832419/ELI/ocean_qxigpo.mp4`}
             autoPlay
             loop
             muted
@@ -78,15 +80,17 @@ export const Hero = () => {
             Learn to <em>Thrive.</em>
           </h2>
           <div className={btn}>
-            <ArrowBtn
+            <Button text='Get Started' path='/#intro' />
+            {/* <ArrowBtn
               text='Get Started'
               arrowColor='var(--accent)'
               lightText={true}
               pageLink={`/#intro`}
-            />
+            /> */}
           </div>
         </div>
       </motion.div>
     </section>
   )
 }
+
