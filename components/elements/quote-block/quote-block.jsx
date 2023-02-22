47dffb44ba3ff0f6ef3dcbea3aa46c3a9ce08740
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-02-22 15:10:46 +0700

    Add quote block component.

---
+++ components/elements/quote-block/quote-block.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+import { QuoteMark } from '../svg/quote-mark'
+
+import {
+  container,
+  quoteMark,
+  text,
+  attr,
+  author as authorStyle,
+  source as sourceStyle,
+} from './quote-block.module.scss'
+
+export const QuoteBlock = ({ quote, author, source }) => {
+  return (
+    <article className={container}>
+      <span className={quoteMark}>
+        <QuoteMark />
+      </span>
+      <p className={text}>{quote}</p>
+      <div className={attr}>
+        <span className={authorStyle}>{author}</span>
+        <span className={sourceStyle}>{source}</span>
+      </div>
+    </article>
+  )
+}
