002250727884a6fd2f3e370b50657c38c04c8ebb
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-03-07 01:53:24 +0700

    Header now optional.

--- ui-components/header-paragraph/header-paragraph.jsx
+++ ui-components/header-paragraph/header-paragraph.jsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 import { container, header, content } from './header-paragraph.module.scss'
 
-export const HeaderParagraph = ({ title, text }) => {
+export const HeaderParagraph = ({ title = null, text }) => {
   return (
     <div className={container}>
-      <h5 className={header}>{title}</h5>
+      {title && <h5 className={header}>{title}</h5>}
       <p className={content}>{text}</p>
     </div>
   )
