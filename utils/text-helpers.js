d666275a1faef18348da73e99874ac224fa389cd
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-04-17 09:44:25 -0700

    Add helper fn for breaking text.

--- utils/text-helpers.js
+++ utils/text-helpers.js
@@ -1,3 +1,12 @@
 export const splitMarkdown = (markdown) => {
   return markdown.split('\n').filter((item) => item !== '')
 }
+
+export const breakWords = (str) => {
+  const [top, bot] = str.split(' ')
+  return (
+    <>
+      {top} <br /> {bot}
+    </>
+  )
+}
