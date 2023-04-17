fb644f4e7fde21e3468e1790fec96c843417b1d4
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-04-17 09:44:44 -0700

    Refactor page for use with contentful.

--- app/page.jsx
+++ app/page.jsx
@@ -1,17 +1,23 @@
+import { useContentful } from 'utils/contentful/useContentful'
+
 import { Contact } from './home/contact/contact'
 import { Hero } from './home/hero/hero'
 import { Intro } from './home/intro/intro'
 import { Pillars } from './home/pillars/pillars'
 import { Testimonial } from './home/testimonial/testimonial'
 
-const Home = () => {
+const Home = async () => {
+  const { content, error } = await useContentful({ key: 'HOME' })
+
+  const components = [Hero, Intro, Pillars, Testimonial, Contact]
+
+  if (error) return <div>Error</div> // TODO: Build error page.
+
   return (
     <>
-      <Hero />
-      <Intro />
-      <Pillars />
-      <Testimonial />
-      <Contact />
+      {components.map((Component, idx) => (
+        <Component key={idx} {...content} />
+      ))}
     </>
   )
 }
