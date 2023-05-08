813b68b03cd521b7e933a3477abdc33208ebbdd1
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-05-08 07:13:08 -0700

    Fix incorrect key name.

--- app/page.config.js
+++ app/page.config.js
@@ -1,4 +1,4 @@
-const CONTENT_KEY = 'FOUNDER'
+const CONTENT_KEY = 'HOME'
 
 const DEFAULT_IMAGE =
   'https://res.cloudinary.com/jameswalker-work/image/upload/f_auto,q_auto:good/v1678126825/ELI/founder-ratioed_jmlezw.png'
