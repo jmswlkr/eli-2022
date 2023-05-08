c66a4b2356d160748def1c63852163c5b70535ba
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-05-08 06:01:25 -0700

    Create route handler for draft mode.

---
+++ app/draft/route.js
@@ -0,0 +1,33 @@
+import { redirect } from 'next/navigation'
+import { draftMode } from 'next/headers'
+import { checkContentful } from 'utils/contentful/checkContentful'
+
+const contentfulSecret = process.env.CONTENTFUL_PREVIEW_SECRET
+
+export async function GET(request) {
+  // Parse query string parameters
+  const { searchParams } = new URL(request.url)
+
+  const secret = searchParams.get('secret')
+
+  const slug = searchParams.get('slug')
+
+  // Check for valid secret and slug
+  if (secret !== contentfulSecret || !slug) {
+    return new Response('Invalid token', { status: 401 })
+  }
+
+  // Check for data existing in contentful
+  const content = await checkContentful({
+    key: slug.toUpperCase(),
+  })
+
+  // error if no data returned
+  if (!content) {
+    return new Response('Invalid slug', { status: 401 })
+  }
+
+  draftMode().enable()
+
+  redirect(`/${slug}`)
+}
