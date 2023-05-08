764d6518886c24fa1859e5c55477e12ba7c7218b
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-05-08 06:00:46 -0700

    Create client for use with preview mode.

--- utils/contentful/config.js
+++ utils/contentful/config.js
@@ -5,3 +5,9 @@ export const client = createClient({
   accessToken: process.env.CONTENTFUL_ACCESS_TOKEN,
   host: 'cdn.contentful.com',
 })
+
+export const previewClient = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_PREVIEW_ACCESS_TOKEN,
+  host: 'preview.contentful.com',
+}) 
