b5bcc870fca77a42ee12b6c18f13eb7b04f6c4d3
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-05-08 07:08:10 -0700

    Add preview mode to home page.

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,5 @@
+import { draftMode } from 'next/headers'
+
 import { useContentful } from 'utils/contentful/useContentful'
 
 import { Contact } from './home/contact/contact'
@@ -5,9 +7,15 @@ import { Hero } from './home/hero/hero'
 import { Intro } from './home/intro/intro'
 import { Pillars } from './home/pillars/pillars'
 import { Testimonial } from './home/testimonial/testimonial'
+import { PAGE_CONFIG } from './page.config'
 
 const Home = async () => {
-  const { content, error } = await useContentful({ key: 'HOME' })
+  const { isEnabled } = draftMode()
+
+  const { content, error } = await useContentful({
+    ...PAGE_CONFIG,
+    preview: isEnabled,
+  })
 
   const components = [Hero, Intro, Pillars, Testimonial, Contact]
 
