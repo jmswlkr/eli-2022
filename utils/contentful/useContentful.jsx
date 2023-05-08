4d314b4b7fe424fac85cee08dde89c3831270c38
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-05-08 06:03:46 -0700

    Update useContentful to optionally use preview mode.

--- utils/contentful/useContentful.jsx
+++ utils/contentful/useContentful.jsx
@@ -3,11 +3,14 @@ import { client } from './config'
 import { CONTENTFUL_CONTENT_KEYS } from './keys'
 
 export const useContentful = async ({
+  preview = false,
   key = 'FOUNDER',
   defaultImage = null,
 }) => {
   try {
-    const entry = await client.getEntry(CONTENTFUL_CONTENT_KEYS[key])
+    const entry = preview 
+    ? await previewClient.getEntry(CONTENTFUL_CONTENT_KEYS[key])
+    : await client.getEntry(CONTENTFUL_CONTENT_KEYS[key])
     const data = entry.fields
     
     if (defaultImage) {
