322ffee2a2a612e43323d4d41fbf4dc47a7f0f28
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-14 01:07:38 -0700

    Remove hero and content pull from template.

--- app/offerings/template.jsx
+++ app/offerings/template.jsx
@@ -1,20 +1,9 @@
-'use client'
-
-import { useSelectedLayoutSegment } from 'next/navigation'
-
-import { offerings } from './offerings-data'
-
-import { PageHero } from 'ui-components/page-hero/page-hero'
 import { ContentLayout } from 'ui-components/content-layout/content-layout'
 
-const OfferingsTemplate = ({ children }) => {
-  const id = useSelectedLayoutSegment()
-
-  const heroContent = offerings[id] ?? offerings.index
+const OfferingsTemplate = async ({ children }) => {
 
   return (
     <ContentLayout>
-      <PageHero {...heroContent} />
       {children}
     </ContentLayout>
   )
