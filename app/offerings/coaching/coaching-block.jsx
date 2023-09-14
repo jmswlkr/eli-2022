5e1c426479e7cc52912a0c762973b7a7dd3c9587
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-14 01:06:09 -0700

    Update with markdown features; styling.

--- app/offerings/coaching/coaching-block.jsx
+++ app/offerings/coaching/coaching-block.jsx
@@ -1,12 +1,22 @@
-import React from 'react'
+import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 
 import { container, header, content } from './coaching-block.module.scss'
 
 export const CoachingBlock = ({ idx, title, text }) => {
+  const formattingOptions = {
+    renderText: (text) => {
+      return text.split('\n').reduce((children, textSegment, index) => {
+        return [...children, index > 0 && <br key={index} />, textSegment]
+      }, [])
+    }
+  }
+
   return (
     <article className={container} data-grid-id={`c${idx}`}>
       <h4 className={header}>{title}</h4>
-      <p className={content}>{text}</p>
+      <p className={content}>
+        {documentToReactComponents(text, formattingOptions)}
+      </p>
     </article>
   )
 }
