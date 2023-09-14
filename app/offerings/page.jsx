499a1ed72ebfefb6e4085e6ba0bbf4bc3ee2f969
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-09-14 02:04:29 -0700

    Add hero section to page, fix missing content.

--- app/offerings/page.jsx
+++ app/offerings/page.jsx
@@ -1,26 +1,36 @@
 import { draftMode } from 'next/headers'
 
-import { OFFERINGS_BLOCKS, OFFERINGS_CONTENT } from './offerings-data'
 import { useContentful } from 'utils/contentful/useContentful'
 import { PAGE_CONFIG } from './page-config'
 import { extractOfferingsContent } from './extract-offerings-content'
 
 import { HeaderParagraph } from 'ui-components/header-paragraph/header-paragraph'
 import { OfferingBlock } from './_components/offering-block/offering-block'
+import { PageHero } from 'ui-components/page-hero/page-hero'
 
 const Offerings = async () => {
   const { isEnabled } = draftMode()
 
   const { content } = await useContentful({
     ...PAGE_CONFIG,
-    preview: isEnabled,
+    preview: isEnabled
   })
+
+  const heroContent = {
+    heroHeaderLines: content.heroHeaderLines,
+    heroImage: content.heroImage,
+    heroButtonText: 'Get Started'
+  }
 
   const offeringsBlocks = extractOfferingsContent(content)
 
   return (
     <>
-      <HeaderParagraph {...content} />
+      <PageHero {...heroContent} />
+      <HeaderParagraph
+        mainContentHeading={content.mainContentHeading}
+        mainContentParagraph={content.mainContentParagraph}
+      />
       {offeringsBlocks.map((block, idx) => {
         return <OfferingBlock key={idx} {...block} />
       })}
