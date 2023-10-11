77c023064f4bec9569c3c98548d81e442c75c39c
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-11 08:16:21 -0700

    Add: Layout gets page load indicator.

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,20 +1,21 @@
+import NextTopLoader from 'nextjs-toploader'
+
+import { ClientLayout } from './layout/client-layout'
+import { Providers } from './(context)/providers'
+
 import '../styles/_mixins.scss'
 import '../styles/global.scss'
 import '../styles/utils.scss'
 import '../styles/variables.scss'
 import '../styles/react-calendar.scss'
 
-import { ClientLayout } from './layout/client-layout'
-import { Providers } from './(context)/providers'
-
 const RootLayout = ({ children }) => {
   return (
     <html lang='en'>
       <body>
+        <NextTopLoader />
         <Providers>
-          <ClientLayout>
-            {children}
-          </ClientLayout>
+          <ClientLayout>{children}</ClientLayout>
         </Providers>
       </body>
     </html>
