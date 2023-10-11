4c8357bade60e581d9e3a3542c553527083c5e1c
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-11 00:12:41 -0700

    Update: Training placeholder page gets hero and cta.

---
+++ app/offerings/training/page-config.js
@@ -0,0 +1,6 @@
+const CONTENT_KEY = 'TRAINING'
+
+const DEFAULT_IMAGE =
+  'https://res.cloudinary.com/jameswalker-work/image/upload/f_auto,q_auto:good/v1678942217/ELI/training--hero_aa43s7.png'
+
+export const PAGE_CONFIG = { key: CONTENT_KEY, defaultImage: DEFAULT_IMAGE }
--- app/offerings/training/page.jsx
+++ app/offerings/training/page.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 
 import { ComingSoon } from 'ui-components/coming-soon/coming-soon'
+import { CtaSection } from 'ui-components/cta-section/cta-section'
+import { PageHero } from 'ui-components/page-hero/page-hero'
+import { PLACEHOLDER_DATA } from './placeholder-data'
+
+const { hero, cta } = PLACEHOLDER_DATA
 
 const CollaborativeLearning = () => {
   return (
     <div>
+      <PageHero {...hero} />
       <ComingSoon />
+      <CtaSection {...cta} />
     </div>
   )
 }
