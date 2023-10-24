0140ebf7939b8fa31fba4752673fa391809546db
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-24 04:53:37 -0700

    Update: Add prop for applying custom styles to content layout.

--- ui-components/content-layout/content-layout.jsx
+++ ui-components/content-layout/content-layout.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 import { container } from './content-layout.module.scss'
 
-export const ContentLayout = ({ children }) => {
-  return <div className={container}>{children}</div>
+export const ContentLayout = ({ children, classes }) => {
+  return <div className={`${container} ${classes}`}>{children}</div>
 }
