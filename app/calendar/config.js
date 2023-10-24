e99037797800ab1b16c03777d0d172062629ad9a
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-24 04:56:41 -0700

    Add: New scheduling components with Calendly.

---
+++ app/calendar/config.js
@@ -0,0 +1,11 @@
+export const calendly_config = {
+  url: 'https://calendly.com/abrzench',
+  pageSettings: {
+    backgroundColor: 'ffffff',
+    hideEventTypeDetails: false,
+    hideLandingPageDetails: false,
+    primaryColor: '235157',
+    textColor: '4d5055'
+  },
+  open: true
+}
