0e26acdbb4818440db20df4a7b21c97533940523
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-31 12:51:59 -0700

    Update: Add basics of blog single page.

--- app/blog/[id]/page.jsx
+++ app/blog/[id]/page.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 
+import { BLOG_CONTENT } from '../data'
+
+import { ContentLayout } from 'ui-components/content-layout/content-layout'
+import { BlogFeatureCard } from '../components/blog-feature-card'
+
+import { container } from './blog-single.module.scss'
+
+
 const BlogSingle = () => {
+  const { feature } = BLOG_CONTENT;
+
   return (
-    <div>Individual blog page.</div>
+    <ContentLayout classes={container}>
+      <BlogFeatureCard content={feature} withMeta={false} />
+    </ContentLayout>
   )
 }
 
-export default BlogSingle
+export default BlogSingle 
