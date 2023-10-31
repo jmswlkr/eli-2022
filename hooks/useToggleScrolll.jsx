ab7da99ce8152c352f96816f4754f1500950b7f0
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2023-10-31 10:21:13 -0700

    Add: Hook for toggling scroll on/off.

---
+++ hooks/useToggleScrolll.jsx
@@ -0,0 +1,18 @@
+import { useEffect, useState } from 'react'
+
+export const useToggleScroll = (isDisabled = false) => {
+  const [scrollDisabled, setScrollDisabled] = useState(null)
+
+  // set with hook arg.
+  useEffect(() => {
+    setScrollDisabled(isDisabled)
+  }, [isDisabled])
+
+  // set with state.
+  useEffect(() => {
+    const html = document.documentElement
+    html.style.overflow = scrollDisabled ? 'hidden' : 'auto'
+  }, [scrollDisabled])
+
+  return { scrollDisabled, setScrollDisabled }
+}
