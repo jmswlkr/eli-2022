58a41ac69a83efcadfef9e37fd08b220d110850e
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-01-10 14:33:55 +0700

    refactor: :lipstick: Update markdown component spacing.
    Use flex gap to add space between paragraphs when using rich text from Contentful.

--- ui-components/header-paragraph/header-paragraph.jsx
+++ ui-components/header-paragraph/header-paragraph.jsx
@@ -3,20 +3,19 @@ import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
 import {
   container,
   header,
-  content as contentStyle,
+  content as contentStyle
 } from './header-paragraph.module.scss'
 
 export const HeaderParagraph = ({
   mainContentHeading,
-  mainContentParagraph,
+  mainContentParagraph
 }) => {
-  
   const formattingOptions = {
     renderText: (text) => {
       return text.split('\n').reduce((children, textSegment, index) => {
         return [...children, index > 0 && <br key={index} />, textSegment]
       }, [])
-    },
+    }
   }
 
   return (
