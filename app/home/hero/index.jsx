7a3ef1f9e7101895c88d5a4d2e293bdd56c2ab7b
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-02-11 21:32:01 +0700

    fix: :bug: Fix hero video broken in mobile.

---
+++ app/home/hero/index.jsx
@@ -0,0 +1,46 @@
+'use client'
+
+import { Button } from '@/ui-components'
+import { AnimatePresence, motion } from 'framer-motion'
+
+export const HeroSection = (props) => {
+  return (
+    <section className='HERO_SECTION isolate hero-container-1 bg-primary-300 relative w-full'>
+      <HeroText />
+      <HeroBG />
+    </section>
+  )
+}
+
+function HeroText() {
+  return (
+    <div className='HERO_TEXT max-w-4/5 full flex-col-center gap-lg relative z-10'>
+      <h1 className='head-1 flex-col-center gap-sm text-center'>
+        <span className='text-white'>Transformational Change</span>
+        <span className='text-[20px] lg:text-[24px] uppercase tracking-[3px] text-white'>
+          Through
+        </span>
+        <span className='text-primary-300'>Embodied Learning</span>
+      </h1>
+      <Button text='Get Started' path="/#intro" classes='solid sm lg:med !bg-primary-300 !border-none'/>
+    </div>
+  )
+}
+
+function HeroBG() {
+  return (
+    <div className='HERO_BG isolate absolute-center z-0 w-screen h-full'>
+      <AnimatePresence>
+        <motion.div className='SHADE bg-gradient-to-r from-primary-500/70 to-accent-300/50 absolute-shade full z-10' />
+        <motion.div className='SHADE_DARK bg-black/40 absolute-shade full z-10' />
+        <video
+          autoPlay
+          loop
+          muted
+          src='https://res.cloudinary.com/jameswalker-work/video/upload/f_auto,q_auto:good/v1674832419/ELI/ocean_qxigpo.mp4'
+          className='VIDEO absolute-img full z-0'
+        ></video>
+      </AnimatePresence>
+    </div>
+  )
+}
--- app/page.jsx
+++ app/page.jsx
@@ -9,16 +9,27 @@ import { FlyerSlider } from './home/flyer-slider'
 import { Intro } from './home/intro/intro'
 import { PillarsSection } from './home/pillars'
 import { Testimonial } from './home/testimonial/testimonial'
+import { HeroSection } from './home/hero'
+import { ContactSection } from './home/contact'
 
 const Home = async () => {
   const { isEnabled } = draftMode()
 
   const { content, error } = await useContentful({
     ...PAGE_CONFIG,
-    preview: isEnabled,
+    preview: isEnabled
   })
 
-  const components = [Hero, FlyerSlider, Intro, PillarsSection, Testimonial, Contact]
+  const components = [
+    // Hero,
+    HeroSection,
+    FlyerSlider,
+    Intro,
+    PillarsSection,
+    Testimonial,
+    // Contact,
+    ContactSection
+  ]
 
   if (error) return <div>Error</div> // TODO: Build error page.
 
