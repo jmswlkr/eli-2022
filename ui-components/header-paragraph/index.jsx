4030c96954d304759d6117965c04ffa74947439f
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-02-28 00:20:44 +0700

    fix: :lipstick: Fix paragraph spacing and add list style in rich text component.

--- ui-components/header-paragraph/index.jsx
+++ ui-components/header-paragraph/index.jsx
@@ -1,13 +1,8 @@
 import { documentToReactComponents } from '@contentful/rich-text-react-renderer'
+import { BLOCKS } from '@contentful/rich-text-types'
 
-import {
-  container,
-  header,
-  content as contentStyle
-} from './header-paragraph.module.scss'
 import { twm } from '@/utils'
 
-// TODO: Rewrite styles with tailwind.
 
 export const HeaderParagraph = ({
   mainContentHeading,
@@ -18,7 +13,12 @@ export const HeaderParagraph = ({
     paragraph: ''
   }
 }) => {
+  console.log("🚀 ~ mainContentParagraph:", mainContentParagraph)
   const formattingOptions = {
+    renderNode: {
+      [BLOCKS.UL_LIST]: (node, children) => <ul>{children}</ul>,
+      [BLOCKS.LIST_ITEM]: (node, children) => <li>{children}</li>
+    },
     renderText: (text) => {
       return text.split('\n').reduce((children, textSegment, index) => {
         return [...children, index > 0 && <br key={index} />, textSegment]
@@ -27,11 +27,29 @@ export const HeaderParagraph = ({
   }
 
   return (
-    <div className={twm('HP_CONTAINER flex-col-tl gap-md', classes.wrapper)}>
-      {mainContentHeading && (
-        <h4 className={twm('HP_HEADER font-body tracking-[1px] head-4 text-primary-500', classes.header)}>{mainContentHeading}</h4>
+    <div
+      className={twm(
+        'HP_CONTAINER flex-col-tl gap-md [&_li]:list-disc [&_li]:ml-[2ch]',
+        classes.wrapper
       )}
-      <p className={twm('HP_PARAGRAPH flex-col-tl [&_br]:inline-block', 'par-1', classes.paragraph)}>
+    >
+      {mainContentHeading && (
+        <h4
+          className={twm(
+            'HP_HEADER font-body tracking-[1px] head-4 text-primary-500',
+            classes.header
+          )}
+        >
+          {mainContentHeading}
+        </h4>
+      )}
+      <p
+        className={twm(
+          'HP_PARAGRAPH flex-col-tl gap-ms [&_br]:inline-block',
+          'par-1',
+          classes.paragraph
+        )}
+      >
         {documentToReactComponents(mainContentParagraph, formattingOptions)}
       </p>
     </div>
