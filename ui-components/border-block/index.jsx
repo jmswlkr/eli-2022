2bf9b5116e94f20fee3efaf78d8c925edf6e3b12
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-05-15 23:49:41 +0700

    refactor(ui-components): :lipstick: Mobile styling adjustments.
    Reduce padding.

--- ui-components/border-block/index.jsx
+++ ui-components/border-block/index.jsx
@@ -3,7 +3,7 @@ import { HeaderParagraph } from '..'
 export const BorderBlock = ({ heading, paragraph }) => {
   return (
     <div
-      className='LIST_ITEM border-primary-500 flex-col-tl gap-ms bg-primary-100 p-md border-l-2 rounded-r-lg'
+      className='LIST_ITEM border-primary-500 flex-col-tl gap-ms bg-primary-100 p-ms md:p-md border-l-2 rounded-r-lg'
     >
       <HeaderParagraph
         mainContentHeading={heading}
