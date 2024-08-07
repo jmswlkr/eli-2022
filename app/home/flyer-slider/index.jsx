0483e4061470396adf130cb53bff2e9b520ec3b5
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-06 23:50:03 -0500

    refactor(home): :recycle: Replace action button with link button. Styling fixes.
    Remove nav dots if only 1 slide. Text color changes to white for better readability. Links now dynamic.

--- app/home/flyer-slider/index.jsx
+++ app/home/flyer-slider/index.jsx
@@ -4,11 +4,18 @@ import Slider from 'react-slick'
 import { useRouter } from 'next/navigation'
 
 import { twm } from '@/utils'
-import { ActionButton, ContentfulImageBlock } from '@/ui-components'
+import {
+  ActionButton,
+  ContentfulImageBlock,
+  LinkButton
+} from '@/ui-components'
+
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL
 
 export const FlyerSlider = ({ showSlider, slider: slides }) => {
   const settings = {
-    dots: true,
+    // dots: true,
+    dots: slides.length > 1,
     infinite: true,
     speed: 500,
     slidesToShow: 1,
@@ -20,10 +27,12 @@ export const FlyerSlider = ({ showSlider, slider: slides }) => {
   if (!showSlider) return null
 
   return (
-    <section className='SLIDER_CONTAINER max-h-[55vh] w-full lg:w-[var(--content-width)] relative'>
+    <section className='SLIDER_CONTAINER max-h-[55vh] w-full lg:w-[var(--content-width)] relative rounded-md overflow-clip'>
       <Slider {...settings}>
         {slides.map((slide) => {
-          return <Slide key={slide.sys.meta} slide={slide.fields} />
+          return (
+            <Slide key={slide.sys.meta} slide={slide.fields} />
+          )
         })}
       </Slider>
     </section>
@@ -40,7 +49,7 @@ function Slide({ slide }) {
       shade: 'bg-primary-300/30'
     },
     light: {
-      title: 'text-primary-300',
+      title: 'text-white',
       button: 'light',
       shade: 'bg-primary-900/50'
     }
@@ -51,28 +60,50 @@ function Slide({ slide }) {
     : colorThemeMap.dark
 
   function handleNavigateToFlyerItem() {
-    router.push(slide.buttonLinkFlyer)
+    router.push(`${baseUrl}${slide.buttonLinkFlyer}`)
   }
 
   return (
-    <article className='SLIDE w-full mb-md h-[40vh] lg:h-[50vh] border relative isolate'>
+    <article className='SLIDE w-full mb-md h-[40vh] lg:h-[50vh] border relative isolate rounded-md overflow-clip'>
       <div className='TEXT_WRAP full p-md gap-md lg:justify-between lg:flex-row lg:items-end relative z-10 flex flex-col items-start justify-end'>
         <div className='flex-col-tl gap-ms'>
-          <p className='meta-1 text-white'>{slide.flyerMetaText}</p>
-          <h2 className={twm('head-1 !font-body', colorTheme.title)}>
+          <p className='meta-1 text-white'>
+            {slide.flyerMetaText}
+          </p>
+          <h2
+            className={twm(
+              'head-1 !font-body',
+              colorTheme.title
+            )}
+          >
             {slide.title}
           </h2>
         </div>
-        <ActionButton
+        <LinkButton
+          path={`${baseUrl}${slide.buttonLinkFlyer}`}
+          text={slide.buttonText}
+          classes={twm('sm solid', colorTheme.button)}
+        />
+        {/* <ActionButton
           text={slide.buttonText}
           classes={twm('sm solid', colorTheme.button)}
           action={handleNavigateToFlyerItem}
-        />
+        /> */}
       </div>
-      <div className='BG_IMAGE_WRAP absolute-center isolate full z-0'>
-        <div className={twm('SHADE absolute-shade z-10', colorTheme.shade)} />
-        <ContentfulImageBlock contentfulImage={slide.image} classes='z-0' />
+      <div className='BG_IMAGE_WRAP absolute-center isolate full overflow-clip z-0'>
+        <div
+          className={twm(
+            'SHADE absolute-shade z-10',
+            colorTheme.shade
+          )}
+        />
+        <ContentfulImageBlock
+          contentfulImage={slide.image}
+          classes='z-0'
+        />
       </div>
     </article>
   )
 }
+
+
