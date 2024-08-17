a4d6195bb9bb114cc748a998217cb3fc5905b65f
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-17 12:48:27 -0500

    refactor(offerings-training): :lipstick: Entire card is linked.
    Wrap card element in link component. Change internal link to button.

--- app/offerings/_components/training-category-card/index.jsx
+++ app/offerings/_components/training-category-card/index.jsx
@@ -1,18 +1,27 @@
 'use client'
 
+import Link from 'next/link'
+
 import { ContentfulImageBlock, LinkButton } from '@/ui-components'
 
 export const TrainingCategoryCard = ({ category }) => {
   return (
-    <div className='CATEGORY_CARD flex-col-center px-ms py-md lg:px-md lg:py-lg rounded-xl gap-ms lg:gap-md bg-primary-500/10 h-auto'>
-      <span className='h-[50px] lg:h-[75px] aspect-square relative'>
-        <ContentfulImageBlock contentfulImage={category.icon} classes='!object-contain'/>
-      </span>
-      <h4 className='HEADING head-4 text-primary-500 text-center'>
-        {category.categoryTitle}
-      </h4>
-      <p className='PARAGRAPH text-center'>{category.blurbDescription}</p>
-      <LinkButton text={'View More →'} path={`/offerings/training/categories/${category.slug}`} classes={'sm text-primary-500 outline'} />
-    </div>
+    <Link className='full flex-col-center' href={`/offerings/training/categories/${category.slug}`}>
+      <div className='CATEGORY_CARD full flex-col-center px-ms py-md lg:px-md lg:py-lg rounded-xl gap-ms lg:gap-md bg-primary-500/10 hover:bg-primary-500/20 h-auto'>
+        <span className='h-[50px] lg:h-[75px] aspect-square relative'>
+          <ContentfulImageBlock
+            contentfulImage={category.icon}
+            classes='!object-contain'
+          />
+        </span>
+        <h4 className='HEADING head-4 text-primary-500 text-center'>
+          {category.categoryTitle}
+        </h4>
+        <p className='PARAGRAPH p-ms text-center'>
+          {category.blurbDescription}
+        </p>
+        <button className='general-btn sm text-primary-500 outline'>View More →</button>
+      </div>
+    </Link>
   )
 }
