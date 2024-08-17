643a65481930c7b119b63f4f8928211c5ac9c662
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-16 21:41:23 -0500

    feat(project): :chart_with_upwards_trend: Add Sentry boilerplate.

---
+++ sentry.server.config.js
@@ -0,0 +1,19 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn: "https://ef200a05b3dae347a0ea46ad0fa1976a@o4507785515761664.ingest.us.sentry.io/4507788204834816",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false,
+
+  // Uncomment the line below to enable Spotlight (https://spotlightjs.com)
+  // spotlight: process.env.NODE_ENV === 'development',
+  
+});
