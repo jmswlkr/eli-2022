505d8c08226b5f9f464f8fa843635d13ef3d2bc5
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-20 13:18:32 -0500

    refactor(home): :lipstick: Update intro header styling.
    Intro header now spans viewport. Use bg color instead of border.

--- app/home/intro/intro.jsx
+++ app/home/intro/intro.jsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import { ReactNode } from 'react'
 
 import {
   ContentfulImageBlock,
@@ -31,16 +32,29 @@ export const Intro = ({
   return (
     <section className={intro}>
       <span className='scroll-pad' id='intro' />
-      <span className='h-[.5px] bg-primary-500 w-full mb-ml' />
-      <p className='text-primary-500  uppercase tracking-[4px] mb-md'>Welcome to ELI</p> 
-      <SectionHeader title={introTitle} classes={ { title: 'head-2 !items-center font-bold' } } />
-      <span className='h-[.5px] bg-primary-500 w-full mt-ms' />
+      <IntroHeader title={introTitle} />
       <div className={cardContainer}>
         {content.introContent.map((card) => {
-          return <IntroCard key={card.sys.id} card={card.fields} />
+          return (
+            <IntroCard key={card.sys.id} card={card.fields} />
+          )
         })}
       </div>
     </section>
+  )
+}
+
+function IntroHeader({ title }) {
+  return (
+    <div className='flex-col-center bg-primary-500/10 gap-md pt-lg pb-md relative w-screen border-t border-b'>
+      <p className='text-primary-500  uppercase tracking-[4px] mb-md'>
+        Welcome to ELI
+      </p>
+      <SectionHeader
+        title={title}
+        classes={{ title: 'head-2 !items-center font-bold' }}
+      />
+    </div>
   )
 }
 
@@ -53,11 +67,15 @@ function IntroCard({ card }) {
             <ContentfulImageBlock contentfulImage={card.image} />
           </div>
           <h4>{card.heading}</h4>
-          <HeaderParagraph mainContentParagraph={card.paragraph} />
+          <HeaderParagraph
+            mainContentParagraph={card.paragraph}
+          />
         </div>
         <div className={cardBtn}>
           {card.buttonText && card.buttonLink && (
-            <button className='general-btn solid sm'>{card.buttonText}</button>
+            <button className='general-btn solid sm'>
+              {card.buttonText}
+            </button>
           )}
         </div>
       </div>
