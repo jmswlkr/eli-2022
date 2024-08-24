dd2a256de4bbdd0f7a91cc76cae046a57a1bb401
JamesMAWalker <44331925+JamesMAWalker@users.noreply.github.com>
2024-08-24 12:58:54 -0500

    refactor(offerings-training): :fire: Remove card blurb.
    Comment out blurb element until final decision is made on its presence.

--- app/offerings/_components/training-category-card/index.jsx
+++ app/offerings/_components/training-category-card/index.jsx
@@ -6,7 +6,10 @@ import { ContentfulImageBlock } from '@/ui-components'
 
 export const TrainingCategoryCard = ({ category }) => {
   return (
-    <Link className='full flex-col-center' href={`/offerings/training/categories/${category.slug}`}>
+    <Link
+      className='full flex-col-center'
+      href={`/offerings/training/categories/${category.slug}`}
+    >
       <div className='CATEGORY_CARD full flex-col-center px-ms py-md lg:px-md lg:py-lg rounded-xl gap-ms lg:gap-md bg-primary-500/10 hover:bg-primary-500/20 h-auto'>
         <span className='h-[50px] lg:h-[75px] aspect-square relative'>
           <ContentfulImageBlock
@@ -17,10 +20,14 @@ export const TrainingCategoryCard = ({ category }) => {
         <h4 className='HEADING head-4 text-primary-500 text-center'>
           {category.categoryTitle}
         </h4>
-        {category.blurbDescription && <p className='PARAGRAPH p-ms text-center'>
-          {category.blurbDescription}
-        </p>}
-        <button className='general-btn sm text-primary-500 outline'>View More →</button>
+        {/* {category.blurbDescription && (
+          <p className='PARAGRAPH p-ms text-center'>
+            {category.blurbDescription}
+          </p>
+        )} */}
+        <button className='general-btn sm text-primary-500 outline'>
+          View Events →
+        </button>
       </div>
     </Link>
   )
